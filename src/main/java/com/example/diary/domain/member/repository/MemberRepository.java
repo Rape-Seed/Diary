ca7de3d0acd5019f7ac7e5624e1f17c67c50e0ab
sangwon <swchoi1997@naver.com>
2022-11-30 00:54:48 +0900

    feat: member repository 생성
    이메일로 맴버 찾는 기능 추가

---
+++ src/main/java/com/example/diary/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.diary.domain.member.repository;
+
+
+import com.example.diary.domain.member.entity.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
