f293defcdf28e4f92d1cdc6aa08372ece35728f4
YuHayeon <youha0930@gmail.com>
2022-12-03 14:45:38 +0900

    add: Diary, Emotion, Member Repository 추가

---
+++ src/main/java/com/example/diary/domain/diary/repository/DiaryRepository.java
@@ -0,0 +1,9 @@
+package com.example.diary.domain.diary.repository;
+
+import com.example.diary.domain.diary.entity.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+}
---
+++ src/main/java/com/example/diary/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.diary.domain.member.repository;
+
+import com.example.diary.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
