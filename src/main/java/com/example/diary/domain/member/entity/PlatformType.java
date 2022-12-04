2ba985cabca05aedb80d700824f27db1bf6c5be3
sangwon <swchoi1997@naver.com>
2022-12-05 01:10:24 +0900

    feat: Platform value값 추가 및 Enum 찾는 함수 추가

--- src/main/java/com/example/diary/domain/member/entity/PlatformType.java
+++ src/main/java/com/example/diary/domain/member/entity/PlatformType.java
@@ -1,7 +1,22 @@
 package com.example.diary.domain.member.entity;
 
+import java.util.Arrays;
+
 public enum PlatformType {
-    GOOGLE,
-    NAVER,
-    KAKAO
+    GOOGLE("google"),
+    NAVER("naver"),
+    KAKAO("kakao");
+
+    private final String platform;
+
+    PlatformType(String platform) {
+        this.platform = platform;
+    }
+
+    public static PlatformType find(String provider) {
+        return Arrays.stream(values())
+                .filter(value -> provider.equals(value.platform))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 잘못된 플랫폼으로 로그인 하였습니다."));
+    }
 }
