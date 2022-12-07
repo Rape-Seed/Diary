14580f61602d60a12d4a2be1ba50e8b67e9631a9
sangwon <swchoi1997@naver.com>
2022-12-07 11:28:56 +0900

    feat: cors properties 추가

---
+++ src/main/java/com/example/diary/global/properties/CorsProperties.java
@@ -0,0 +1,22 @@
+package com.example.diary.global.properties;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class CorsProperties {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${cors.allowed-methods}")
+    private String allowedMethods;
+
+    @Value("${cors.allowed-headers}")
+    private String allowedHeaders;
+
+    @Value("${cors.max-age}")
+    private Long maxAge;
+}
