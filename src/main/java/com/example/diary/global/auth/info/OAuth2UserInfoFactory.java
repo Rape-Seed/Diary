c4a1b9af0e3903c545c3cdf74caa7f5c7ee0c91a
sangwon <swchoi1997@naver.com>
2022-12-07 09:52:15 +0900

    feat: 소셜정보 매핑 및 가져올 정보 로직 추가

---
+++ src/main/java/com/example/diary/global/auth/info/OAuth2UserInfoFactory.java
@@ -0,0 +1,18 @@
+package com.example.diary.global.auth.info;
+
+import static com.example.diary.domain.member.entity.PlatformType.GOOGLE;
+
+import com.example.diary.domain.member.entity.PlatformType;
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(PlatformType platformType, Map<String, Object> attributes) {
+        if (platformType == GOOGLE) {
+            return new GoogleOAuth2UserInfo(attributes);
+        }
+
+        throw new IllegalArgumentException("[ERROR] Invalid PlatForm Info");
+    }
+
+}
