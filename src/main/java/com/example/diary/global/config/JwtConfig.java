45f21269852a5892375af13b45cb4d05fdc66ac6
sangwon <swchoi1997@naver.com>
2022-12-07 09:51:13 +0900

    feat: Bean으로 등록 및 토큰 주입 시 인코딩하는 로직 추가

--- src/main/java/com/example/diary/global/config/JwtConfig.java
+++ src/main/java/com/example/diary/global/config/JwtConfig.java
@@ -1,9 +1,12 @@
 package com.example.diary.global.config;
 
 import com.example.diary.global.auth.token.AuthTokenProvider;
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JwtConfig {
 
     @Value("${spring.jwt.secretKey}")
@@ -11,6 +14,6 @@ public class JwtConfig {
 
     @Bean
     public AuthTokenProvider authTokenProvider() {
-        return new AuthTokenProvider(secretKey);
+        return new AuthTokenProvider(Base64.getEncoder().encodeToString(secretKey.getBytes()));
     }
 }
