ef22fb3f3e7ae13bd0a42c5596404ae3371d0d4d
sangwon <swchoi1997@naver.com>
2022-12-09 20:54:12 +0900

    feat: 401에러시 수행 로직 추가

---
+++ src/main/java/com/example/diary/global/auth/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.example.diary.global.auth.exception;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        log.info("Responding with unauthorized error. Message = {}",
+                authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getLocalizedMessage());
+    }
+}
+
