08c0141f633867a729b885354202c6ac151dd032
YuHayeon <youha0930@gmail.com>
2022-12-10 13:59:14 +0900

    refactor: Exception 정책 변경

--- src/main/java/com/example/diary/global/advice/ExceptionAdvice.java
+++ src/main/java/com/example/diary/global/advice/ExceptionAdvice.java
@@ -1,7 +1,11 @@
 package com.example.diary.global.advice;
 
+import com.example.diary.global.advice.exception.DiaryNotAuthorizedException;
+import com.example.diary.global.advice.exception.DiaryNotFoundException;
+import com.example.diary.global.advice.exception.DiaryWrongDateException;
 import com.example.diary.global.advice.exception.LoginFailureException;
 import com.example.diary.global.advice.exception.MemberNotFoundException;
+import com.example.diary.global.advice.exception.TokenValidFailedException;
 import com.example.diary.global.advice.exceptionDto.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,36 +18,36 @@ public class ExceptionAdvice {
     @ExceptionHandler(LoginFailureException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse loginFailureException() {
-        return ExceptionResponse.getFailureResult(-102, "아이디 혹은 비밀번호가 틀립니다.");
+        return ExceptionResponse.getFailureResult(-101, "아이디 혹은 비밀번호가 틀립니다.");
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse memberNotFoundException() {
-        return ExceptionResponse.getFailureResult(-103, "해당 멤버를 찾을 수 없습니다.");
+        return ExceptionResponse.getFailureResult(-102, "해당 멤버를 찾을 수 없습니다.");
     }
 
     @ExceptionHandler(TokenValidFailedException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse tokenValidFailedException() {
-        return ExceptionResponse.getFailureResult(-104, "Failed to generate Token.");
+        return ExceptionResponse.getFailureResult(-103, "Failed to generate Token.");
     }
 
     @ExceptionHandler(DiaryNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse diaryNotFoundException() {
-        return ExceptionResponse.getFailureResult(-104, "해당 일기를 찾을 수 없습니다.");
+        return ExceptionResponse.getFailureResult(-201, "해당 일기를 찾을 수 없습니다.");
     }
 
     @ExceptionHandler(DiaryNotAuthorizedException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse diaryNotAuthorizedException() {
-        return ExceptionResponse.getFailureResult(-105, "해당 일기의 접근 권한이 없습니다.");
+        return ExceptionResponse.getFailureResult(-202, "해당 일기의 접근 권한이 없습니다.");
     }
 
     @ExceptionHandler(DiaryWrongDateException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse diaryWrongDateException() {
-        return ExceptionResponse.getFailureResult(-106, "해당 날짜의 일기를 작성할 수 없습니다.");
+        return ExceptionResponse.getFailureResult(-203, "해당 날짜의 일기를 작성할 수 없습니다.");
     }
 }
