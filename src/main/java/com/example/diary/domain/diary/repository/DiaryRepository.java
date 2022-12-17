970fe08259d66c6e5052e47fc737706cc0cdb51a
YuHayeon <youha0930@gmail.com>
2022-12-17 17:06:42 +0900

    feat: team, member, date로 다이어리 찾기 추가

--- src/main/java/com/example/diary/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/example/diary/domain/diary/repository/DiaryRepository.java
@@ -1,9 +1,14 @@
 package com.example.diary.domain.diary.repository;
 
 import com.example.diary.domain.diary.entity.Diary;
+import com.example.diary.domain.member.entity.Member;
+import com.example.diary.domain.team.entity.Team;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long>, DiaryRepositoryCustom {
+
+    Diary findDiaryByTeamAndMemberAndDate(Team team, Member member, LocalDate date);
 }
