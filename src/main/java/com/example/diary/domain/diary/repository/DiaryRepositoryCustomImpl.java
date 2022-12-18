2958b8e20c71d934fed2128cc65439f43b0d97f3
YuHayeon <youha0930@gmail.com>
2022-12-18 14:13:48 +0900

    fix(Diary): 공유일기 id로 조회에서 fetch join으로 teamMembers도 가져오도록 수정

--- src/main/java/com/example/diary/domain/diary/repository/DiaryRepositoryCustomImpl.java
+++ src/main/java/com/example/diary/domain/diary/repository/DiaryRepositoryCustomImpl.java
@@ -3,6 +3,7 @@ package com.example.diary.domain.diary.repository;
 import static com.example.diary.domain.diary.entity.QDiary.diary;
 import static com.example.diary.domain.member.entity.QMember.member;
 import static com.example.diary.domain.team.entity.QTeam.team;
+import static com.example.diary.domain.team.entity.QTeamMember.teamMember;
 
 import com.example.diary.domain.diary.entity.Diary;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -19,6 +20,7 @@ public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
         return queryFactory.selectFrom(diary)
                 .join(diary.member, member).fetchJoin()
                 .join(diary.team, team).fetchJoin()
+                .join(team.teamMembers, teamMember).fetchJoin()
                 .where(diary.id.eq(diaryId))
                 .fetchOne();
     }
