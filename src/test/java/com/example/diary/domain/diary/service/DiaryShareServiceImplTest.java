7c8695c1b6f6071703adc240deac7bc6acfcdca6
YuHayeon <youha0930@gmail.com>
2022-12-18 14:45:50 +0900

    test(Diary): 공통적으로 사용하는 객체는 BeforeEach로 만들도록 변경

--- src/test/java/com/example/diary/domain/diary/service/DiaryShareServiceImplTest.java
+++ src/test/java/com/example/diary/domain/diary/service/DiaryShareServiceImplTest.java
@@ -6,6 +6,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import com.example.diary.domain.diary.dto.DiaryResponse;
 import com.example.diary.domain.diary.entity.Diary;
 import com.example.diary.domain.diary.repository.DiaryRepository;
+import com.example.diary.domain.emotion.entity.Emotion;
+import com.example.diary.domain.emotion.entity.EmotionType;
+import com.example.diary.domain.emotion.repository.EmotionRepository;
 import com.example.diary.domain.member.entity.Member;
 import com.example.diary.domain.member.repository.MemberRepository;
 import com.example.diary.domain.team.entity.AcceptStatus;
@@ -35,34 +38,40 @@ class DiaryShareServiceImplTest {
     @Autowired
     DiaryRepository diaryRepository;
     @Autowired
+    EmotionRepository emotionRepository;
+    @Autowired
     DiaryShareServiceImpl diaryShareService;
 
-    private Member me;
     private Member member1;
+    private Member member2;
     private Team team1;
     private Team team2;
-    private TeamMember teamMember;
+    private TeamMember teamMember1;
+    private TeamMember teamMember2;
+    private Emotion emotion;
+    private Diary diary1;
+    private Diary diary2;
 
     @BeforeEach
     void setUp() {
-        Member newMember = Member.builder()
+        Member newMember1 = Member.builder()
                 .name("테스트")
                 .birthday(LocalDate.of(2022, 1, 1))
                 .email("test@test.com")
                 .code("QWER12")
                 .build();
-        me = memberRepository.save(newMember);
+        member1 = memberRepository.save(newMember1);
 
-        Member newMember1 = Member.builder()
+        Member newMember2 = Member.builder()
                 .name("테스트2")
                 .birthday(LocalDate.of(2022, 1, 2))
                 .email("test2@test.com")
                 .code("QWER13")
                 .build();
-        member1 = memberRepository.save(newMember1);
+        member2 = memberRepository.save(newMember2);
 
         Team newTeam1 = Team.builder()
-                .name("테스트 팀")
+//                .name("테스트 팀")
                 .startDate(LocalDate.of(2022, 1, 1))
                 .endDate(LocalDate.of(2022, 1, 3))
                 .build();
@@ -75,55 +84,63 @@ class DiaryShareServiceImplTest {
                 .build();
         team2 = teamRepository.save(newTeam2);
 
-        TeamMember newTeamMember = TeamMember.builder()
+        TeamMember newTeamMember1 = TeamMember.builder()
                 .team(team1)
-                .member(me)
+                .member(member1)
                 .acceptStatus(AcceptStatus.APPROVE)
                 .build();
-        teamMember = teamMemberRepository.save(newTeamMember);
-        teamMember.setMember(me);
-        teamMember.setTeam(team1);
+        teamMember1 = teamMemberRepository.save(newTeamMember1);
+        teamMember1.setMember(member1);
+        teamMember1.setTeam(team1);
+
+        TeamMember newTeamMember2 = TeamMember.builder()
+                .team(team1)
+                .member(member2)
+                .acceptStatus(AcceptStatus.APPROVE)
+                .build();
+        teamMember2 = teamMemberRepository.save(newTeamMember2);
+        teamMember2.setMember(member2);
+        teamMember2.setTeam(team1);
+
+        Emotion newEmotion = Emotion.builder()
+                .content(EmotionType.HAPPY)
+                .build();
+        emotion = emotionRepository.save(newEmotion);
+
+        Diary newDiary1 = Diary.builder()
+                .content("오늘 행복했다.")
+                .date(LocalDate.of(2022, 1, 2))
+                .member(member1)
+                .team(team1)
+                .emotion(emotion)
+                .build();
+        diary1 = diaryRepository.save(newDiary1);
+
+        Diary newDiary2 = Diary.builder()
+                .content("오늘 슬펐다.")
+                .date(LocalDate.of(2022, 1, 2))
+                .member(member2)
+                .team(team1)
+                .emotion(emotion)
+                .build();
+        diary2 = diaryRepository.save(newDiary2);
     }
 
     @DisplayName("올바른 공유일기를 조회")
     @Test
     void getSharedDiary_success() throws Exception {
         //given
-        TeamMember teamMember2 = TeamMember.builder()
-                .team(team1)
-                .member(member1)
-                .acceptStatus(AcceptStatus.APPROVE)
-                .build();
-        TeamMember saveTeamMember2 = teamMemberRepository.save(teamMember2);
-        saveTeamMember2.setMember(member1);
-        saveTeamMember2.setTeam(team1);
-
-        Diary newDiary = Diary.builder()
-                .content("오늘 행복했다.")
-                .date(LocalDate.of(2022, 1, 2))
-                .member(me)
-                .team(team1)
-                .build();
-        diaryRepository.save(newDiary);
-
-        Diary diary2 = Diary.builder()
-                .content("오늘 슬펐다.")
-                .date(LocalDate.of(2022, 1, 2))
-                .member(member1)
-                .team(team1)
-                .build();
-        Diary saveDiary2 = diaryRepository.save(diary2);
-
         DiaryResponse diaryResponse = DiaryResponse.builder()
-                .diaryId(saveDiary2.getId())
+                .diaryId(diary2.getId())
                 .teamName(team1.getName())
-                .memberName(member1.getName())
-                .content(saveDiary2.getContent())
-                .date(saveDiary2.getDate())
+                .memberName(member2.getName())
+                .content(diary2.getContent())
+                .date(diary2.getDate())
+                .emotion(emotion.getContent().toString())
                 .build();
 
         //when
-        DiaryResponse sharedDiaryResponse = diaryShareService.getSharedDiary(saveDiary2.getId(), me);
+        DiaryResponse sharedDiaryResponse = diaryShareService.getSharedDiary(diary2.getId(), member1);
 
         //then
         assertThat(sharedDiaryResponse).isEqualTo(diaryResponse);
@@ -133,33 +150,34 @@ class DiaryShareServiceImplTest {
     @Test
     void getSharedDiary_NotTeamMember() throws Exception {
         //given
-        TeamMember teamMember2 = TeamMember.builder()
+        Member newMember3 = Member.builder()
+                .name("테스트3")
+                .birthday(LocalDate.of(2022, 1, 2))
+                .email("test3@test.com")
+                .code("QWER14")
+                .build();
+        Member member3 = memberRepository.save(newMember3);
+
+        TeamMember newTeamMember3 = TeamMember.builder()
                 .team(team2)
-                .member(member1)
+                .member(member3)
                 .acceptStatus(AcceptStatus.APPROVE)
                 .build();
-        TeamMember saveTeamMember2 = teamMemberRepository.save(teamMember2);
-        saveTeamMember2.setMember(member1);
-        saveTeamMember2.setTeam(team2);
+        TeamMember teamMember3 = teamMemberRepository.save(newTeamMember3);
+        teamMember3.setMember(member3);
+        teamMember3.setTeam(team2);
 
-        Diary newDiary = Diary.builder()
-                .content("오늘 행복했다.")
-                .date(LocalDate.of(2022, 1, 2))
-                .member(me)
-                .team(team1)
-                .build();
-        Diary saveDiary = diaryRepository.save(newDiary);
-
-        Diary diary2 = Diary.builder()
+        Diary newDiary3 = Diary.builder()
                 .content("오늘 슬펐다.")
                 .date(LocalDate.of(2022, 1, 2))
-                .member(member1)
+                .member(member3)
                 .team(team2)
+                .emotion(emotion)
                 .build();
-        Diary saveDiary2 = diaryRepository.save(diary2);
+        Diary diary3 = diaryRepository.save(newDiary3);
 
         //then
-        assertThatThrownBy(() -> diaryShareService.getSharedDiary(saveDiary2.getId(), me))
+        assertThatThrownBy(() -> diaryShareService.getSharedDiary(diary2.getId(), member3))
                 .isInstanceOf(DiaryNotAuthorizedException.class);
     }
 
@@ -167,25 +185,25 @@ class DiaryShareServiceImplTest {
     @Test
     void getSharedDiary_NotWroteMyDiary() throws Exception {
         //given
-        TeamMember teamMember2 = TeamMember.builder()
+        Member newMember3 = Member.builder()
+                .name("테스트3")
+                .birthday(LocalDate.of(2022, 1, 2))
+                .email("test3@test.com")
+                .code("QWER14")
+                .build();
+        Member member3 = memberRepository.save(newMember3);
+
+        TeamMember newTeamMember3 = TeamMember.builder()
                 .team(team1)
-                .member(member1)
+                .member(member3)
                 .acceptStatus(AcceptStatus.APPROVE)
                 .build();
-        TeamMember saveTeamMember2 = teamMemberRepository.save(teamMember2);
-        saveTeamMember2.setMember(member1);
-        saveTeamMember2.setTeam(team1);
-
-        Diary diary2 = Diary.builder()
-                .content("오늘 슬펐다.")
-                .date(LocalDate.of(2022, 1, 3))
-                .member(member1)
-                .team(team1)
-                .build();
-        Diary saveDiary2 = diaryRepository.save(diary2);
+        TeamMember teamMember3 = teamMemberRepository.save(newTeamMember3);
+        teamMember3.setMember(member3);
+        teamMember3.setTeam(team1);
 
         //then
-        assertThatThrownBy(() -> diaryShareService.getSharedDiary(saveDiary2.getId(), me))
+        assertThatThrownBy(() -> diaryShareService.getSharedDiary(diary2.getId(), member3))
                 .isInstanceOf(DiaryNotAuthorizedException.class);
     }
 
@@ -193,35 +211,10 @@ class DiaryShareServiceImplTest {
     @Test
     void getSharedDiary_NotAcceptTeamMember() throws Exception {
         //given
-        teamMember.updateAcceptStatus(AcceptStatus.REJECT);
-
-        TeamMember teamMember2 = TeamMember.builder()
-                .team(team1)
-                .member(member1)
-                .acceptStatus(AcceptStatus.APPROVE)
-                .build();
-        TeamMember saveTeamMember2 = teamMemberRepository.save(teamMember2);
-        saveTeamMember2.setMember(member1);
-        saveTeamMember2.setTeam(team1);
-
-        Diary newDiary = Diary.builder()
-                .content("오늘 행복했다.")
-                .date(LocalDate.of(2022, 1, 2))
-                .member(me)
-                .team(team1)
-                .build();
-        Diary saveDiary = diaryRepository.save(newDiary);
-
-        Diary diary2 = Diary.builder()
-                .content("오늘 슬펐다.")
-                .date(LocalDate.of(2022, 1, 2))
-                .member(member1)
-                .team(team1)
-                .build();
-        Diary saveDiary2 = diaryRepository.save(diary2);
+        teamMember1.updateAcceptStatus(AcceptStatus.REJECT);
 
         //then
-        assertThatThrownBy(() -> diaryShareService.getSharedDiary(saveDiary2.getId(), me))
+        assertThatThrownBy(() -> diaryShareService.getSharedDiary(diary2.getId(), member1))
                 .isInstanceOf(DiaryNotAuthorizedException.class);
     }
 }
