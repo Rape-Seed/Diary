6b58e374dc149b76d14f3bdb0b968d9a7ddea5fd
yuha <youha0930@gmail.com>
2022-12-21 01:17:15 +0900

    feat(Team): 팀 삭제시 팀멤버도 함께 삭제되도록 Cascade 추가

--- src/main/java/com/example/diary/domain/team/entity/Team.java
+++ src/main/java/com/example/diary/domain/team/entity/Team.java
@@ -4,6 +4,7 @@ import com.example.diary.global.common.BaseEntity;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -28,7 +29,7 @@ public class Team extends BaseEntity {
     private String code;
 
     @Builder.Default
-    @OneToMany(mappedBy = "team")
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
     private List<TeamMember> teamMembers = new ArrayList<>();
 
     private LocalDate startDate;
