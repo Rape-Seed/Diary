d9dfd583e93e6fe37831464f136f187a65efbbd9
yuha <youha0930@gmail.com>
2022-12-24 02:02:37 +0900

    feat(Diary): DiaryRequest에 scope 추가

--- src/main/java/com/example/diary/domain/diary/dto/DiaryRequest.java
+++ src/main/java/com/example/diary/domain/diary/dto/DiaryRequest.java
@@ -3,17 +3,35 @@ package com.example.diary.domain.diary.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import lombok.Data;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DiaryRequest {
 
     private String content;
 
-    private Long teamId;
+    private Scope scope;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate date;
 
     private LocalDateTime currentTime;
+
+    @Getter
+    public static class Scope {
+
+        private boolean isPersonal;
+
+        private List<Long> teams;
+    }
+
 }
