b9f707dbb23289cbcee68afb9ef669a59451b1d9
sangwon <swchoi1997@naver.com>
2022-12-26 10:20:18 +0900

    fix: data어노테이션 제거

--- src/main/java/com/example/diary/domain/member/dto/MyInfoRequestDto.java
+++ src/main/java/com/example/diary/domain/member/dto/MyInfoRequestDto.java
@@ -1,12 +1,31 @@
 package com.example.diary.domain.member.dto;
 
 import lombok.Builder;
-import lombok.Data;
 
-@Data
 @Builder
 public class MyInfoRequestDto {
     private String name;
     private String birthday;
     private String profileImage;
+
+    public MyInfoRequestDto() {
+    }
+
+    public MyInfoRequestDto(String name, String birthday, String profileImage) {
+        this.name = name;
+        this.birthday = birthday;
+        this.profileImage = profileImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
 }
