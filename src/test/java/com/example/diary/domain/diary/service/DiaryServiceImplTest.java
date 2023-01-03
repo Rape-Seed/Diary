46872396fbc9ab32f4cfb53da06fedbdb9058782
yuha <youha0930@gmail.com>
2023-01-03 22:15:07 +0900

    test(Diary): 일기 생성 테스트
    개인일기 생성
    공유일기 생성
    개인일기, 공유일기 동시 생성
    팀이 아닌 경우 예외처리
    날짜 오류시 예외처리

--- src/test/java/com/example/diary/domain/diary/service/DiaryServiceImplTest.java
+++ src/test/java/com/example/diary/domain/diary/service/DiaryServiceImplTest.java
@@ -3,6 +3,9 @@ package com.example.diary.domain.diary.service;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import com.example.diary.domain.diary.dto.DiaryCreateResponseDto;
+import com.example.diary.domain.diary.dto.DiaryRequest;
+import com.example.diary.domain.diary.dto.DiaryRequest.Scope;
 import com.example.diary.domain.diary.dto.DiaryUpdateRequest;
 import com.example.diary.domain.diary.entity.Diary;
 import com.example.diary.domain.diary.repository.DiaryRepository;
@@ -11,8 +14,16 @@ import com.example.diary.domain.emotion.entity.EmotionType;
 import com.example.diary.domain.emotion.repository.EmotionRepository;
 import com.example.diary.domain.member.entity.Member;
 import com.example.diary.domain.member.repository.MemberRepository;
+import com.example.diary.domain.team.entity.AcceptStatus;
+import com.example.diary.domain.team.entity.Team;
+import com.example.diary.domain.team.entity.TeamMember;
+import com.example.diary.domain.team.repository.TeamMemberRepository;
+import com.example.diary.domain.team.repository.TeamRepository;
 import com.example.diary.global.advice.exception.DiaryNotAuthorizedException;
+import com.example.diary.global.advice.exception.WrongDateException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,6 +39,10 @@ class DiaryServiceImplTest {
     @Autowired
     MemberRepository memberRepository;
     @Autowired
+    TeamRepository teamRepository;
+    @Autowired
+    TeamMemberRepository teamMemberRepository;
+    @Autowired
     DiaryRepository diaryRepository;
     @Autowired
     EmotionRepository emotionRepository;
@@ -41,6 +56,8 @@ class DiaryServiceImplTest {
     private Member member2;
     private Emotion emotion;
     private Diary diary1;
+    private Team team1;
+    private TeamMember teamMember1;
 
     @BeforeEach
     void setUp() {
@@ -72,6 +89,22 @@ class DiaryServiceImplTest {
                 .emotion(emotion)
                 .build();
         diary1 = diaryRepository.save(newDiary1);
+
+        Team newTeam1 = Team.builder()
+                .name("테스트 팀")
+                .startDate(LocalDate.of(2022, 1, 1))
+                .endDate(LocalDate.of(2022, 1, 3))
+                .build();
+        team1 = teamRepository.save(newTeam1);
+
+        TeamMember newTeamMember1 = TeamMember.builder()
+                .team(team1)
+                .member(member1)
+                .acceptStatus(AcceptStatus.APPROVE)
+                .build();
+        teamMember1 = teamMemberRepository.save(newTeamMember1);
+        teamMember1.setMember(member1);
+        teamMember1.setTeam(team1);
     }
 
     @DisplayName("정상적으로 일기를 수정한다.")
@@ -124,5 +157,118 @@ class DiaryServiceImplTest {
         assertThatThrownBy(() -> diaryService.delete(diary1, member2))
                 .isInstanceOf(DiaryNotAuthorizedException.class);
         assertThat(true).isEqualTo(diaryRepository.findById(diaryId).isPresent());
+    }
+
+    @DisplayName("개인일기를 정상적으로 생성한다.")
+    @Test
+    void createPersonalDiary_success() throws Exception {
+        //given
+        String content = "테스트 content";
+        DiaryRequest diaryRequest = DiaryRequest.builder()
+                .content(content)
+                .scope(new Scope(true))
+                .currentTime(LocalDateTime.of(2022, 1, 1, 10, 13))
+                .date(LocalDate.of(2022, 1, 1))
+                .build();
+        //when
+        DiaryCreateResponseDto diaryCreateResponseDto = diaryService.create(diaryRequest, member1);
+
+        //then
+        assertThat(content).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getContent());
+        assertThat(LocalDate.of(2022, 1, 1)).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getDate());
+    }
+
+    @DisplayName("공유일기를 정상적으로 생성한다.")
+    @Test
+    void createSharedDiary_success() throws Exception {
+        //given
+        String content = "테스트 content";
+        DiaryRequest diaryRequest = DiaryRequest.builder()
+                .content(content)
+                .scope(new Scope(List.of(team1.getId())))
+                .currentTime(LocalDateTime.of(2022, 1, 1, 10, 13))
+                .date(LocalDate.of(2022, 1, 1))
+                .build();
+        //when
+        DiaryCreateResponseDto diaryCreateResponseDto = diaryService.create(diaryRequest, member1);
+
+        //then
+        assertThat(content).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getContent());
+        assertThat(LocalDate.of(2022, 1, 1)).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getDate());
+        assertThat(team1.getName()).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getTeamName());
+    }
+
+    @DisplayName("개인일기와 공유일기를 정상적으로 생성한다.")
+    @Test
+    void createSharedAndPersonalDiary_success() throws Exception {
+        //given
+        String content = "테스트 content";
+        DiaryRequest diaryRequest = DiaryRequest.builder()
+                .content(content)
+                .scope(new Scope(true, List.of(team1.getId())))
+                .currentTime(LocalDateTime.of(2022, 1, 1, 10, 13))
+                .date(LocalDate.of(2022, 1, 1))
+                .build();
+
+        //when
+        DiaryCreateResponseDto diaryCreateResponseDto = diaryService.create(diaryRequest, member1);
+
+        //then
+        assertThat(2).isEqualTo(diaryCreateResponseDto.getDiaries().size());
+        assertThat(content).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getContent());
+        assertThat(LocalDate.of(2022, 1, 1)).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getDate());
+    }
+
+    @DisplayName("해당 팀 멤버가 아닌 경우 공유일기 생성시 예외처리가 발생한다.")
+    @Test
+    void createSharedDiary_notTeamMember() throws Exception {
+        //given
+        String content = "테스트 content";
+        DiaryRequest diaryRequest = DiaryRequest.builder()
+                .content(content)
+                .scope(new Scope(List.of(team1.getId())))
+                .currentTime(LocalDateTime.of(2022, 1, 1, 10, 13))
+                .date(LocalDate.of(2022, 1, 1))
+                .build();
+        //then
+        assertThatThrownBy(() -> diaryService.create(diaryRequest, member2))
+                .isInstanceOf(DiaryNotAuthorizedException.class);
+    }
+
+    @DisplayName("다음날 전일 일기를 작성할 경우 정상적으로 일기를 작성한다.")
+    @Test
+    void createSharedDiary_rightDate() throws Exception {
+        //given
+        String content = "테스트 content";
+        DiaryRequest diaryRequest = DiaryRequest.builder()
+                .content(content)
+                .scope(new Scope(List.of(team1.getId())))
+                .currentTime(LocalDateTime.of(2022, 1, 2, 10, 13))
+                .date(LocalDate.of(2022, 1, 1))
+                .build();
+
+        //when
+        DiaryCreateResponseDto diaryCreateResponseDto = diaryService.create(diaryRequest, member1);
+
+        //then
+        assertThat(content).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getContent());
+        assertThat(content).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getContent());
+        assertThat(LocalDate.of(2022, 1, 1)).isEqualTo(diaryCreateResponseDto.getDiaries().get(0).getDate());
+    }
+
+    @DisplayName("이틀 전 일기를 작성할 경우 예외처리가 발생한다.")
+    @Test
+    void createSharedDiary_wrongDate() throws Exception {
+        //given
+        String content = "테스트 content";
+        DiaryRequest diaryRequest = DiaryRequest.builder()
+                .content(content)
+                .scope(new Scope(List.of(team1.getId())))
+                .currentTime(LocalDateTime.of(2022, 1, 3, 10, 13))
+                .date(LocalDate.of(2022, 1, 1))
+                .build();
+        //then
+        assertThatThrownBy(() -> diaryService.create(diaryRequest, member2))
+                .isInstanceOf(WrongDateException.class);
     }
 }
