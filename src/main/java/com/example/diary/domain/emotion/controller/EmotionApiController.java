371d5229d8802f76467d181fe14b535d73dc2d02
sangwon <swchoi1997@naver.com>
2023-01-03 17:20:43 +0900

    feat: 감정분석 서비스 controller추가

---
+++ src/main/java/com/example/diary/domain/emotion/controller/EmotionApiController.java
@@ -0,0 +1,27 @@
+package com.example.diary.domain.emotion.controller;
+
+import com.example.diary.domain.emotion.dto.EmotionRequestDto;
+import com.example.diary.domain.emotion.dto.EmotionResponseDto;
+import com.example.diary.domain.emotion.service.EmotionService;
+import com.example.diary.global.common.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class EmotionApiController {
+
+    private final EmotionService emotionService;
+
+    @PostMapping("/v1/emotion")
+    public ResponseDto<EmotionResponseDto> analyzeMyDiaryEmotion(@RequestBody EmotionRequestDto emotionRequestDto) {
+        return new ResponseDto<>(emotionService.analyzeDiary(emotionRequestDto), "감정을 분석했습니다.", HttpStatus.OK);
+    }
+}
+
+
