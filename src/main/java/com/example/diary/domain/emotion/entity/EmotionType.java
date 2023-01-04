cca07ffcff650bf1191250d5f09be8174e8bf9cc
sangwon <swchoi1997@naver.com>
2023-01-04 22:37:58 +0900

    feat: 감정 enum 변경 및 조회 로직 추가

--- src/main/java/com/example/diary/domain/emotion/entity/EmotionType.java
+++ src/main/java/com/example/diary/domain/emotion/entity/EmotionType.java
@@ -1,16 +1,46 @@
 package com.example.diary.domain.emotion.entity;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.Getter;
 
 @Getter
 public enum EmotionType {
-    HAPPY("행복"),
-    SOSO("중간"),
-    SAD("슬픔"),
-    NONE("판독불가");
+    HAPPY("positive"),
+    SO("neutral"),
+    SAD("negative"),
+    NONE("none");
 
-    private String message;
+    public static final Map<String, EmotionType> TYPES = Collections.unmodifiableMap(
+            Stream.of(values()).collect(Collectors.toMap(EmotionType::getMessage, Function.identity()))
+    );
+
+    public Map<String, EmotionType> getTypes() {
+        return TYPES;
+    }
+
+    private final String message;
 
     EmotionType(String message) {
+        this.message = message;
+    }
+
+    public static String myEmotion(String sentiment) {
+        if (!TYPES.containsKey(sentiment.toLowerCase())) {
+            return NONE.name();
+        }
+        return TYPES.get(sentiment.toLowerCase()).name();
+
+    }
+
+    public static EmotionType find(String sentiment) {
+        return Arrays.stream(values())
+                .filter(emotionType -> emotionType.getMessage().equals(sentiment.toLowerCase()))
+                .findFirst()
+                .orElse(NONE);
     }
 }
