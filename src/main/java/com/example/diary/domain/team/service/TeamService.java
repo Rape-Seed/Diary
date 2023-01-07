ab8814bdc78b5f620530022aee7cda1d2a362e06
yuha <youha0930@gmail.com>
2023-01-07 23:04:00 +0900

    feat(Team): team 초대 기능에 요청보낸 사람도 team에 추가

--- src/main/java/com/example/diary/domain/team/service/TeamService.java
+++ src/main/java/com/example/diary/domain/team/service/TeamService.java
@@ -1,5 +1,6 @@
 package com.example.diary.domain.team.service;
 
+import com.example.diary.domain.member.entity.Member;
 import com.example.diary.domain.team.dto.TeamInviteRequest;
 import com.example.diary.domain.team.dto.TeamInviteResponse;
 import com.example.diary.domain.team.dto.TeamReplyRequest;
@@ -7,7 +8,7 @@ import com.example.diary.domain.team.dto.TeamReplyResponse;
 
 public interface TeamService {
 
-    TeamInviteResponse inviteTeam(TeamInviteRequest teamInviteRequest);
+    TeamInviteResponse inviteTeam(TeamInviteRequest teamInviteRequest, Member member);
 
     TeamReplyResponse replyTeam(TeamReplyRequest teamReplyRequest);
 }
