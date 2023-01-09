013eb5cb859da7b497dc2bcf73ae1e8f90a49c76
sangwon <swchoi1997@naver.com>
2023-01-09 22:39:35 +0900

    feat: 문구로 phrase객체 찾아오는 로직 추가

--- src/main/java/com/example/diary/domain/recommend/repository/PhraseRepository.java
+++ src/main/java/com/example/diary/domain/recommend/repository/PhraseRepository.java
@@ -4,4 +4,5 @@ import com.example.diary.domain.recommend.entity.Phrase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhraseRepository extends JpaRepository<Phrase, Long>, PhraseRepositoryCustom {
+    Phrase findByContent(String content);
 }
