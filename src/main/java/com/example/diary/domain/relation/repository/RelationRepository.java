a0737f9a9a9347d09a4981d582c4f8f13096f170
sangwon <swchoi1997@naver.com>
2023-01-12 21:39:30 +0900

    feat: JpaRepository에 querydsl 구현한 repo상속

--- src/main/java/com/example/diary/domain/relation/repository/RelationRepository.java
+++ src/main/java/com/example/diary/domain/relation/repository/RelationRepository.java
@@ -3,5 +3,5 @@ package com.example.diary.domain.relation.repository;
 import com.example.diary.domain.relation.entity.Relation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RelationRepository extends JpaRepository<Relation, Long> {
+public interface RelationRepository extends JpaRepository<Relation, Long>, CustomRelationRepository {
 }
