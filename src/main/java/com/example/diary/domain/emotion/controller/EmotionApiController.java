e4a23fe16652285b0e850eb66b01bff2671478e4
sangwon <swchoi1997@naver.com>
2023-01-12 11:11:02 +0900

    feat: TODO 추

--- src/main/java/com/example/diary/domain/emotion/controller/EmotionApiController.java
+++ src/main/java/com/example/diary/domain/emotion/controller/EmotionApiController.java
@@ -3,6 +3,8 @@ package com.example.diary.domain.emotion.controller;
 import com.example.diary.domain.emotion.dto.EmotionRequestDto;
 import com.example.diary.domain.emotion.dto.EmotionResponseDto;
 import com.example.diary.domain.emotion.service.EmotionService;
+import com.example.diary.domain.member.entity.CurrentMember;
+import com.example.diary.domain.member.entity.Member;
 import com.example.diary.global.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,9 +21,19 @@ public class EmotionApiController {
     private final EmotionService emotionService;
 
     @PostMapping("/v1/emotion")
-    public ResponseDto<EmotionResponseDto<?>> analyzeMyDiaryEmotion(@RequestBody EmotionRequestDto emotionRequestDto) {
+    public ResponseDto<EmotionResponseDto<?>> analyzeMyDiaryEmotion(
+            @RequestBody EmotionRequestDto emotionRequestDto,
+            @CurrentMember Member member) {
         return new ResponseDto<>(emotionService.analyzeDiary(emotionRequestDto), "감정을 분석했습니다.", HttpStatus.OK);
     }
+
+    /**
+     * TODO 감정 분석 시 다이어리 랑 1대1 매핑해주기 -> test, 상원 <- 거의 완료
+     * TODO 분석한 감정 한달단위로 가져오기 - 하연
+     * TODO 월별, 주별 감정분석 리포트 - batch, 상원
+     * TODO 팀구성, 친구추가 알림메세지 - 상원
+     * TODO 일기 적으라는 알람 설정 - batch, 상원
+     */
 }
 
 
